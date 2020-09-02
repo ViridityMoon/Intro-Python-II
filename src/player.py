4a51a3b9263be9e34fdfe4dd36e3165daa28385b
Noah Green <viriditymoon@gmail.com>
2020-09-02 18:19:43 -0400

    Player and Room classes started; finished Room. Added comments for further understanding

--- src/player.py
+++ src/player.py
@@ -1,2 +1,13 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+# Class Player Encapsulation
+class Player:
+    # init function: takes in name and a current_room; current_room
+    # will initialize to the "outside" room
+    def __init__(self, name, current_room="outside"):
+        # Player's name (type: String)
+        self.name = name
+        # Player's current_room (type: Room Object)
+        self.current_room = current_room
+        
--- src/room.py
+++ src/room.py
@@ -1,2 +1,25 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+# Class Room Encapsulation
+class Room:
+    # init function: takes in name and description
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        # cardinal directions: initiated to none,
+        # will hold the full Room classes connected to this one
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
+
+    # Override __str___ method
+    def __str__(self):
+        return f"Room Name: {self.name}\n{self.description}"
+
+# Testing functionality of Room class instantiation
+r = Room("Name", "Description")
+
+# Further testing with __str__ method
+print(r)
